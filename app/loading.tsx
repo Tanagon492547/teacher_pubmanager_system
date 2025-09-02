f97f604899b17b15700ed69cfa28d1976d6adc9a
Tanagon492547 <tanagon04092547@gmail.com>
2025-09-02 22:33:45 +0700

    feat: 🎸 เพิ่ม ICON โหลด

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,4 @@
 export default function Loading() {
   // Or a custom loading skeleton component
-  return <p>Loading...</p>
+  return <p>Loading... <span className="loading loading-spinner loading-xl"></span> </p>
 }
