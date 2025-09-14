e770252944c9d55d15d3ebd8522b5a36b484c213
Rattanathorn <6610210341@psu.ac.th>
2025-09-14 19:25:24 +0700

    feat: 🎸 อัปเดตหน้าแสดงรายละเอียดบทความและ layout หลัก

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Navbar from "@/components/navigations/Navbar";
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode; }>) => {
   const pathname = usePathname();
-  const userType = ''; // เปลี่ยนเป็น 'teacher' , 'staff' 'admin' ตามที่ต้องการ
+  const userType = 'teacher'; // เปลี่ยนเป็น 'teacher' , 'staff' 'admin' ตามที่ต้องการ
 
   const login = false; // เปลี่ยนเป็น true เพื่อแสดง Navbar
 
